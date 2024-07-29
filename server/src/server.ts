e8aa3700cd849d8b9d5934333bf1cdd6e9e20a38
Aristides Pascoal Queta <aristidesquetajr@gmail.com>
2024-07-29 16:54:01 +0100

    feat(server): configured cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,17 @@
 import fastify from 'fastify'
-import { routes } from './routes'
 import {
   serializerCompiler,
   validatorCompiler
 } from 'fastify-type-provider-zod'
 
+import cors from '@fastify/cors'
+import { routes } from './routes'
+
 const app = fastify()
+
+app.register(cors, {
+  origin: `${process.env.WEB_BASE_URL}`
+})
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
